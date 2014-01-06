7931c558a4de271e89bec2e6ce15d2f8f74dbf35
Xiaowei Song <dawnwei.song@gmail.com>
2014-01-05 21:47:46 -0500

    even using line scan speedup, it is still exceeding time limit

--- peking/1009/src.cpp
+++ peking/1009/src.cpp
@@ -42,49 +42,92 @@ int main(int argc, char **argv){
 
         int iRLE=0;  //track RLE array, cumcdf start from 1
         int po,oo;   //pixel position
-        for(int i=0;i<height;i++)
-        for(int j=0;j<width;j++){
-            //map pixel value to RLE index
-            po=i*width+j; while(po>=cumLength[iRLE] && iRLE<=nl)iRLE++;
-            int maxdiff=0;
-            int pv=value[iRLE-1];//image[i*width+j]
-            //cerr<<po<<" "<<pv<<endl;
-            //
 
-            //Same RLE detection
-            //if top-left and bottom-right both in same RLE
-            //then no need to calclate any more
-
-            for(int ki=i-1;ki<=i+1;ki++){
-                if(ki<0 || ki>=height)continue;
-                for(int kj=j-1;kj<=j+1;kj++){
-                    if(ki==i && kj==j) continue;
-                    if(kj<0 || kj>=width)continue;
-
-                    oo=ki*width+kj;
-                    int oRLE=iRLE-1;//assume not far away from iRLE
-                    if(oo<po){
-                        while(oo <cumLength[oRLE] && oRLE>=0)oRLE--;
+        for(int i=0;i<height;i++){
+            if(i){//speedup
+            //cerr<<i<<endl;
+            //Assumption: RLE is far more larger than image's width
+            //Same RLE 2 lines ignorance if 4 lines are in same RLE
+            int pon0, pon1, pon2;
+            int n0rle=iRLE;
+            pon0=(i-1)*width;
+            while(pon0<cumLength[n0rle] && n0rle>=0)n0rle--;
+            //cerr<< "pon0: "<<pon0<<"|"<<n0rle <<" "<< cumLength[n0rle]<<" "<<n0rle+1<<","<<nl<<" "<<cumLength[n0rle+1]<<endl;
+            //if(n0rle==iRLE){
+            if(pon0>=cumLength[n0rle] && (n0rle+1)<=nl &&pon0<cumLength[n0rle+1]){
+                //int n1rle=n0rle;
+                pon1=(i+1)*width-1;
+                //while(pon1>=cumLength[n1rle] && n1rle<=nl)n1rle++; n1rle-=1;
+                //cerr<< "pon1: "<<pon1<<"|"<<n0rle <<" "<< cumLength[n0rle]<<" "<<n0rle+1<<","<<nl<<" "<<cumLength[n0rle+1]<<endl;
+                if(pon1>=cumLength[n0rle] && (n0rle+1)<=nl &&pon1<cumLength[n0rle+1]){
+                    //int n2rle=n1rle;
+                    pon2=(i+2)*width-1;
+                    //while(pon2>=cumLength[n2rle] && n2rle<=nl)n2rle++; n2rle-=1;
+                    //if(n2rle==n1rle){
+                    //cerr<< "pon2: "<<pon2<<"|"<<n0rle <<" "<< cumLength[n0rle]<<" "<<n0rle+1<<","<<nl<<" "<<cumLength[n0rle+1]<<endl;
+                    if(pon2>=cumLength[n0rle] && (n0rle+1)<=nl &&pon2<cumLength[n0rle+1]){
+                        if(0==oldPV){
+                            oldPN+=width;
+                            //ignore 1 lines from now
+                            //i+=1;//consider i++ in for loop, compensation
+                            //calculate directly the result from the cumLength edge point
+                            int speedo=1;
+                            pon2+=width*speedo;
+                            while(pon2>=cumLength[n0rle] && (n0rle+1)<=nl &&pon2<cumLength[n0rle+1]){
+                            oldPN+=width*speedo;
+                            i+=1*speedo;
+                            pon2+=width*speedo;
+                            speedo++;
+                            }
+                            speedo--;
+                            oldPN-=width*speedo;
+                            i-=1*speedo;
+                            continue;
+                        }
                     }
-                    else{
-                        while(oo >=cumLength[oRLE] && oRLE<=nl)oRLE++;
-                        oRLE-=1;
-                    }
-                    if(oRLE==iRLE-1)break;//same RLE
-
-                    int ov=value[oRLE] ;//image[ki*width+kj]
-                    int diff = pv>ov?(pv-ov):(ov-pv);
-                    if(diff>maxdiff)maxdiff=diff;
                 }
-            }//rimage[i*width+j]=maxdiff;
-            //detect if RLE output is needed
-            if(oldPV!=maxdiff){
-                if(oldPV>=0)//consider init condition
-                    cout<<oldPV<<" "<<oldPN  <<endl;
-                //start new count
-                oldPV=maxdiff;
-                oldPN=1;
-            }else{ oldPN++; }
+            }//n0rle==iRLE
+            }//i>=1
+
+            for(int j=0;j<width;j++){
+                //map pixel value to RLE index
+                po=i*width+j; while(po>=cumLength[iRLE] && iRLE<=nl)iRLE++; iRLE-=1;
+
+                int maxdiff=0;
+                int pv=value[iRLE];//image[i*width+j]
+                //cerr<<po<<" "<<pv<<endl;
+                //
+
+                for(int ki=i-1; ki<=i+1; ki++){
+                    if(ki<0 || ki>=height)continue;
+                    for(int kj=j-1;kj<=j+1;kj++){
+                        if(ki==i && kj==j) continue;
+                        if(kj<0 || kj>=width)continue;
+
+                        oo=ki*width+kj;
+                        int oRLE=iRLE;//assume not far away from iRLE
+                        if(oo<po){
+                            while(oo <cumLength[oRLE] && oRLE>=0)oRLE--;
+                        }
+                        else{
+                            while(oo >=cumLength[oRLE] && oRLE<=nl)oRLE++;
+                            oRLE-=1;
+                        }
+
+                        int ov=value[oRLE] ;//image[ki*width+kj]
+                        int diff = pv>ov?(pv-ov):(ov-pv);
+                        if(diff>maxdiff)maxdiff=diff;
+                    }
+                }//rimage[i*width+j]=maxdiff;
+                //detect if RLE output is needed
+                if(oldPV!=maxdiff){
+                    if(oldPV>=0)//consider init condition
+                        cout<<oldPV<<" "<<oldPN  <<endl;
+                    //start new count
+                    oldPV=maxdiff;
+                    oldPN=1;
+                }else{ oldPN++; }
+            }
         }
         //for end condition
         cout<<oldPV<<" "<<oldPN  <<endl;
